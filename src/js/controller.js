5c8c60457e06bffcb6a7a89cb9f4fdf78715f269
Brian Bycynski <high5bri@hotmail.com>
2020-12-19 03:36:44 -0500

    Initial Commit

---
+++ src/js/controller.js
@@ -0,0 +1,36 @@
+//
+//
+
+import * as model from './model';
+import recipeView from './views/recipeView';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const recipeContainer = document.querySelector('.recipe');
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipe = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1) Loading Recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering Recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+['hashchange', 'load'].forEach((event) =>
+  window.addEventListener(event, controlRecipe)
+);
