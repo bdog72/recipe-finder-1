2b21d7b40974be870c7680d6bb9b35f3d89dbd8e
Brian Bycynski <high5bri@hotmail.com>
2020-12-21 01:57:02 -0500

    implementing Pagination

---
+++ practice/app.js
@@ -0,0 +1,107 @@
+//
+//
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+// javascriptIsFun = 'YES';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(year);
+// console.log(typeof year);
+
+// ***********************************************
+// **********************************************÷
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+
+// var job = 'programmer';
+// job = 'teacher';
+
+// ***********************************************
+// **********************************************÷
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann';
+// console.log(firstName + ' ' + lastName);
+
+// let x = 10 + 5;
+// console.log(x);
+
+// x += 10;
+// console.log(x);
+
+// x *= 4;
+// console.log(x);
+
+// x++;
+// console.log(x);
+
+// console.log(ageJonas > ageSarah);
+// console.log(ageJonas < ageSarah);
+
+// console.log(ageSarah >= 18);
+// const isFullAge = ageSarah >= 18;
+
+// console.log(isFullAge);
+// console.log(now - 1991 > now - 2018);
+
+// ***********************************************
+// **********************************************÷
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// const averageAge = (ageJonas + ageSarah) / 2;
+// console.log(ageJonas, ageSarah);
+
+// console.log(averageAge);
+// console.log(now - 1991 > now - 2018);
+
+// let x, y;
+// x = y = 25 - 10 - 5;
+// console.log(x, y);
+
+// ***********************************************
+// ***********************************************
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const BMIMark = massMark / heightMark ** 2;
+// console.log(BMIMark);
+
+// const BMIJohn = massJohn / heightJohn ** 2;
+// console.log(BMIJohn);
+
+// const markHigherBMI = BMIMark > BMIJohn;
+// console.log(markHigherBMI);
+
+// ***********************************************
+// **********************************************÷
+
+// ***********************************************
+// **********************************************÷
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -6,64 +6,10 @@ import View from './View';
 import icons from 'url:../../img/icons.svg';
 import { Fraction } from 'fractional';
 
-class RecipeView {
+class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
-  _data;
   _errorMessage = 'We could not find that recipe. Please try another one';
   _message = '';
-
-  render(data) {
-    this._data = data;
-    const markup = this._generateMarkup();
-    this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
-
-  _clear() {
-    this._parentElement.innerHTML = '';
-  }
-
-  renderSpinner() {
-    const markup = `
-      <div class="spinner">
-        <svg>
-          <use href="${icons}#icon-loader"></use>
-        </svg>
-      </div> 
-    `;
-    this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
-
-  renderError(message = this._errorMessage) {
-    const markup = `
-      <div class="error">
-        <div>
-          <svg>
-            <use href="${icons}#icon-alert-triangle"></use>
-          </svg>
-        </div>
-        <p>${message}</p>
-      </div>
-    `;
-    this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
-
-  renderMessage(message = this._message) {
-    const markup = `
-      <div class="message">
-        <div>
-          <svg>
-            <use href="${icons}#icon-smile"></use>
-          </svg>
-        </div>
-        <p>${message}</p>
-      </div>
-    `;
-    this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
 
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach((event) =>
@@ -116,62 +62,63 @@ class RecipeView {
       </div>
 
       <div class="recipe__user-generated">
-        <svg>
-          <use href="${icons}#icon-user"></use>
-        </svg>
       </div>
       <button class="btn--round">
-        <svg class="">
-          <use href="${icons}#icon-bookmark-fill"></use>
-        </svg>
+      <svg class="">
+      <use href="${icons}#icon-bookmark-fill"></use>
+      </svg>
       </button>
-    </div>
-
-    <div class="recipe__ingredients">
+      </div>
+      
+      <div class="recipe__ingredients">
       <h2 class="heading--2">Recipe ingredients</h2>
       <ul class="recipe__ingredient-list">
       ${this._data.ingredients.map(this._generateMarkupIngredient).join('')}
-    </div>
-
-    <div class="recipe__directions">
+      </div>
+      
+      <div class="recipe__directions">
       <h2 class="heading--2">How to cook it</h2>
       <p class="recipe__directions-text">
-        This recipe was carefully designed and tested by
-        <span class="recipe__publisher">${
-          this._data.publisher
-        }</span>. Please check out
+      This recipe was carefully designed and tested by
+      <span class="recipe__publisher">${
+        this._data.publisher
+      }</span>. Please check out
         directions at their website.
-      </p>
-      <a
+        </p>
+        <a
         class="btn--small recipe__btn"
         href="${this._data.sourceUrl}"
         target="_blank"
-      >
+        >
         <span>Directions</span>
         <svg class="search__icon">
-          <use href="${icons}#icon-arrow-right"></use>
+        <use href="${icons}#icon-arrow-right"></use>
         </svg>
-      </a>
-    </div>
-  `;
+        </a>
+        </div>
+        `;
   }
 
   _generateMarkupIngredient(ing) {
     return `
-      <li class="recipe__ingredient">
-        <svg class="recipe__icon">
-          <use href="${icons}#icon-check"></use>
-        </svg>
-        <div class="recipe__quantity">${
-          ing.quantity ? new Fraction(ing.quantity).toString() : ''
-        }</div>
+    <li class="recipe__ingredient">
+    <svg class="recipe__icon">
+    <use href="${icons}#icon-check"></use>
+    </svg>
+    <div class="recipe__quantity">${
+      ing.quantity ? new Fraction(ing.quantity).toString() : ''
+    }</div>
         <div class="recipe__description">
-          <span class="recipe__unit">${ing.unit}</span>
-          ${ing.description}
+        <span class="recipe__unit">${ing.unit}</span>
+        ${ing.description}
         </div>
-      </li>
-    `;
+        </li>
+        `;
   }
 }
 
 export default new RecipeView();
+
+// <svg>
+//   <use href="${icons}#icon-user"></use>
+// </svg>
