7fbf962ec9bae9aadbc2bf5ab021643bfb306c72
Brian Bycynski <high5bri@hotmail.com>
2021-01-11 02:57:18 -0500

    now can login to each dummy account

--- projects/1-bankist/script.js
+++ projects/1-bankist/script.js
@@ -7,7 +7,7 @@
 /////////////////////////////////////////////////
 // BANKIST APP
 
-// console.log(123456);
+console.log(123456);
 
 // Data
 const account1 = {
@@ -87,7 +87,7 @@ const displayMovements = function (movements) {
   });
 };
 
-displayMovements(account1.movements);
+// displayMovements(account1.movements);
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => {
@@ -96,10 +96,10 @@ const calcDisplayBalance = function (movements) {
   labelBalance.textContent = `＄${balance} USD`;
 };
 
-calcDisplayBalance(account1.movements);
+// calcDisplayBalance(account1.movements);
 
-const calcDisplaySummary = function (movements) {
-  const incomes = movements
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
     .filter(mov => {
       return mov > 0;
     })
@@ -108,7 +108,7 @@ const calcDisplaySummary = function (movements) {
     }, 0);
   labelSumIn.textContent = `＄${incomes}`;
 
-  const out = movements
+  const out = acc.movements
     .filter(mov => {
       return mov < 0;
     })
@@ -117,12 +117,13 @@ const calcDisplaySummary = function (movements) {
     }, 0);
   labelSumOut.textContent = `＄${Math.abs(out)}`;
 
-  const interest = movements
+  const interest = acc.movements
     .filter(mov => {
       return mov > 0;
     })
     .map(deposit => {
-      return (deposit * 1.2) / 100;
+      return (deposit * acc.interestRate) / 100;
+      // return (deposit * 1.2) / 100;
     })
     .filter((int, i, arr) => {
       return int >= 1;
@@ -133,7 +134,7 @@ const calcDisplaySummary = function (movements) {
   labelSumInterest.textContent = `＄${interest}`;
 };
 
-calcDisplaySummary(account1.movements);
+// calcDisplaySummary(account1.movements);
 
 const createUserNames = function (accs) {
   accs.forEach(function (acc) {
@@ -149,23 +150,93 @@ const createUserNames = function (accs) {
 
 createUserNames(accounts);
 
+// Event Handler
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+  currentAccount = accounts.find(account => {
+    return account.username === inputLoginUsername.value;
+  });
+  // inputLoginUsername.value = '';
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and Message
+    labelWelcome.textContent = `Welcome Back ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    // Clear Fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+    // Display Movements
+    displayMovements(currentAccount.movements);
+
+    // Display Balance
+    calcDisplayBalance(currentAccount.movements);
+
+    // Display Summary
+    calcDisplaySummary(currentAccount);
+  }
+  // inputLoginPin.value = '';
+});
+
 // LECTURES
 /////////////////////////////////////////////////
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const euroToUsd = 1.1;
-const bozo = movements
-  .filter(mov => {
-    return mov > 0;
-  })
-  .map(mov => mov * euroToUsd)
-  .reduce((acc, curr) => {
-    return acc + curr;
-  }, 0);
+// const firstWithdraw = movements.find(function (mov) {
+//   return mov < 0;
+// });
 
-console.log(bozo);
+// console.log(firstWithdraw);
+// console.log(accounts);
 
+// const account = accounts.find(function (acc) {
+//   return acc.owner === `Adam Apple`;
+// });
+
+// console.log(account);
+
+// for (const account of accounts) {
+//   if (account.owner === 'Adam Apple') {
+//     console.log(account);
+//   } else {
+//     console.log(`Doh`);
+//   }
+// console.log(account);
+// account.owner === 'Adam Apple';
+// }
+
+// const euroToUsd = 1.1;
+// const bozo = movements
+//   .filter(mov => {
+//     return mov > 0;
+//   })
+//   .map(mov => mov * euroToUsd)
+//   .reduce((acc, curr) => {
+//     return acc + curr;
+//   }, 0);
+
+// console.log(bozo);
+
+// const calcAverageHumanAge = ages => {
+//   const humanAges = ages.map(age => {
+//     return age <= 2 ? 2 * age : 16 + age * 4;
+//   });
+//   const adults = humanAges.filter(age => {
+//     return age >= 18;
+//   });
+//   const average =
+//     adults.reduce((acc, curr) => {
+//       return acc + curr;
+//     }, 0) / adults.length;
+
+//   return average;
+// };
 // const calcAverageHumanAge = ages => {
 //   const humanAges = ages.map(age => {
 //     return age <= 2 ? 2 * age : 16 + age * 4;
